1793e11420fb14985eaa9042c62870b69eed1ce2
williamsd4808 <williamsd4808@ufl.edu>
2018-07-07 11:50:31 -0500

    Created a module folder and a test file for it. The module folder contains only one module, printModule.py, and the moduleTester tests each function in the module to date. Comments to be added later.

---
+++ moduleTester.py
@@ -0,0 +1,7 @@
+from modules.printModule import *
+printNoNewLine("<STRING1>The next string should be printed on the same line</STRING1>")
+print("<STRING2>This is string 2</STRING2>\n")
+
+myString = "This is the string to be reversed"
+print(myString)
+print(reverseString(myString))
---
+++ modules/printModule.py
@@ -0,0 +1,6 @@
+def printNoNewLine(string):
+	print(string, end = "")
+
+def reverseString(string):
+	tempString = string[::-1]
+	return tempString
