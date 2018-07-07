4c7d9b4dbe8441c732180dd8b60f6eed693a5ee9
Rjkerlee <rj.kerlee@gmail.com>
2018-07-07 10:58:08 -0500

    (LISTEN!)

--- fizz1.py
+++ fizz1.py
@@ -1,11 +1,8 @@
 for fizzbuzz in range(50):
-	if fizzbuzz % 3 ==0 and fizzbuzz % 5 == 0:
-		print(str(fizzbuzz) + "fizzbuzz",end='')
-		continue
-	elif fizzbuzz % 3 == 0:
+	if fizzbuzz % 3 == 0:
 		print(str(fizzbuzz) + "fizz",end='')
 		continue
-	elif fizzbuzz % 5 ==0:
+	if fizzbuzz % 5 ==0:
 		print(str(fizzbuzz) + "buzz",end='')
 		continue
 	print(fizzbuzz,end='')
