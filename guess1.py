5f93fce2f989b70d702dfdf404750d785ffbef72
williamsd4808 <williamsd4808@ufl.edu>
2018-07-14 09:52:26 -0500

    Identation errors fixed

--- guess1.py
+++ guess1.py
@@ -2,66 +2,46 @@
 
 import random
 
- 
+guessesTaken = 0
 
-  guessesTaken = 0
+print('Hello! What is your name?')
 
- 
+myName = input()
 
-  print('Hello! What is your name?')
+number = random.randint(1, 20)
 
-  myName = input()
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
- 
+while guessesTaken < 6:
 
-  number = random.randint(1, 20)
+  print('Take a guess.')
 
- print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+  guess = input()
 
+  guess = int(guess)
 
+  guessesTaken = guessesTaken + 1
 
-  while guessesTaken < 6:
+  if guess < number:
 
-     print('Take a guess.') # There are four spaces in front of print.
+    print('Your guess is too low.')
 
-     guess = input()
+  if guess > number:
 
-     guess = int(guess)
+    print('Your guess is too high.')
 
+  if guess == number:
 
+    break
 
-     guessesTaken = guessesTaken + 1
+if guess == number:
 
+  guessesTaken = str(guessesTaken)
 
+  print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
 
-     if guess < number:
+if guess != number:
 
-         print('Your guess is too low.') # There are eight spaces in front of print.
+  number = str(number)
 
-
-
-     if guess > number:
-
-         print('Your guess is too high.')
-
-
-
-     if guess == number:
-
-         break
-
-
-
- if guess == number:
-
-     guessesTaken = str(guessesTaken)
-
-30.     print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
-
-31.
-
-32. if guess != number:
-
-33.     number = str(number)
-
-34.     print('Nope. The number I was thinking of was ' + number)
+  print('Nope. The number I was thinking of was ' + number)
